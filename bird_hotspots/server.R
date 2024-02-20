27e90e37f1fa3a15b414af1e40c13c2afe7a6cda
Ben Best <ben@ecoquants.com>
2024-02-20 15:47:48 -0800

    + bird_hotspots app

--- bird_hotspots/server.R
+++ bird_hotspots/server.R
@@ -1,20 +1,32 @@
-library(shiny)
-
-# Define server logic required to draw a histogram
+# Define server logic
 function(input, output, session) {
 
-  output$distPlot <- renderPlot({
+  sel_cells <- reactiveValues()
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  pal <- colorNumeric(
+    palette = "inferno",
+    domain  = hhvh$numhot)
 
-    # draw the histogram with the specified number of bins
-    hist(
-      x, breaks = bins, col = 'darkgray', border = 'white',
-      xlab = 'Waiting time to next eruption (in mins)',
-      main = 'Histogram of waiting times')
+  m = get_basemap() |>
+    # https://rstudio.github.io/leaflet/articles/choropleths.html
+    addPolygons(
+      data         = hhvh,
+      fillColor    = ~pal(numhot), fillOpacity = 0.7,
+      color        = "white",      opacity     = 0.9, weight = 0.5,
+      smoothFactor = 0.5)
 
+  # https://github.com/r-spatial/mapedit/blob/35ae8ccfb90b682bf98a82e85c3047f098857db3/inst/examples/shiny_modules.R#L46C1-L71C20
+  drawn <- callModule(editMod, "map_select", m)
+
+  observe({
+    req(drawn()$finished)
+    sel_cells$intersection <- st_intersection(drawn()$finished, hhvh)
+  })
+
+  output$map_out <- renderLeaflet({
+    req(sel_cells$intersection)
+
+    (mapview(sel_cells$intersection) + mapview(drawn()$finished))@map
   })
 
 }
--- bird_hotspots/ui.R
+++ bird_hotspots/ui.R
@@ -1,21 +1,5 @@
-library(shiny)
-
-# Define UI for application that draws a histogram
 fluidPage(
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        "bins",
-        "Number of bins:",
-        min = 1,
-        max = 50,
-        value = 30)),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot") ) ) )
+  fluidRow(
+    column(6, editModUI("map_select")),
+    column(6, leafletOutput("map_out")) ) )
